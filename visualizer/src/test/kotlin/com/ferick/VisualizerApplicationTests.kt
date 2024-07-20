99bb12e6142007f8e3405e6d0ccb1b64b8342ca6
Cuchillon <ferickadmin@gmail.com>
2024-07-20 18:29:30 +0300

    Init visualizer

---
+++ visualizer/src/test/kotlin/com/ferick/VisualizerApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.ferick
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VisualizerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+}
